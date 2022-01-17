db6c71262358df3159a2c38a965304a16c590d69
Algonacci <algonacci1@gmail.com>
2022-01-18 03:06:52 +0700

    tugas 11

---
+++ Tugas-10-SanberAppFlutter/sanberappflutter/lib/Tugas/Tugas11/telegram.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Telegram extends StatefulWidget {
+  const Telegram({Key? key}) : super(key: key);
+
+  @override
+  _TelegramState createState() => _TelegramState();
+}
+
+class _TelegramState extends State<Telegram> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Telegram'),
+      ),
+    );
+  }
+}
